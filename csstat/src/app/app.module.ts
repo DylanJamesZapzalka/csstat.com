ce08e4e653f92772f8166bf6d7719a93c152675a
Dylan Zapzalka <dylanjameszapzalka@hotmail.com>
2021-03-29 03:01:55 -0500

    quick change

--- csstat/src/app/app.module.ts
+++ csstat/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { GraphsComponent } from './graphs/graphs.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    GraphsComponent
   ],
   imports: [
     BrowserModule
