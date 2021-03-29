1a21ca50f88700ab20da930c7d6580effc4792bc
Dylan Zapzalka <dylanjameszapzalka@hotmail.com>
2021-03-29 16:38:23 -0500

    Testing more graphs functionality

--- csstat/src/app/graphs/graphs.component.ts
+++ csstat/src/app/graphs/graphs.component.ts
@@ -22,6 +22,20 @@ export class GraphsComponent implements OnInit {
       "options": {is3D: true},
       "width": 550,
       "height": 400
+    },
+    {"title": "Default Example Chart",
+    "type": "Histogram", 
+    "data": [
+      ['Firefox', 45.0],
+      ['IE', 26.8],
+      ['Chrome', 12.8],
+      ['Safari', 8.5],
+      ['Opera', 6.2],
+      ['Others', 0.7] ],
+      "columnNames": ['Browser', 'Percentage'],
+      "options": {is3D: true},
+      "width": 550,
+      "height": 400
     }
   ];
 
