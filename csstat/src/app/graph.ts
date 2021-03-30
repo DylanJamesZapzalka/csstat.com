775d3dcbebdd163c3771e6bc9bac1914568c486c
Dylan Zapzalka <dylanjameszapzalka@hotmail.com>
2021-03-29 23:35:20 -0500

    Minimal

--- csstat/src/app/graph.ts
+++ csstat/src/app/graph.ts
@@ -1,2 +1,11 @@
+import { Title } from "@angular/platform-browser";
+
 export class Graph {
+    title: string;
+    type: string;
+    data: any;
+    columnNames: any;
+    options: any;
+    width: number;
+    height: number;
 }
