1c5610d5c0cc23f8d7f15f59e56be1985d193da0
daniel-w-johnson <daniel.w.johnson@ndsu.edu>
2021-03-30 19:23:02 -0500

    added html and basic ts functionality for calculations/results section

--- csstat/src/app/calculations/calculations.component.ts
+++ csstat/src/app/calculations/calculations.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calculations.component.css']
 })
 export class CalculationsComponent implements OnInit {
-  calculations: string[] = ["mean", "median", "mode","standard deviation", "variance","p-value","correlation between parameters", "percentile of point"];
+  //calculations: string[] = [];
+  results: number[] = [12, 123, 22, 5.78, 12, 123, 22, 5.78];
+  calculations: {[key: string]: number}={"mean": 1, "median": 4, "mode": 2.3,"standard deviation": 4, "variance": 0,"p-value": 6,"correlation between parameters": 8, "percentile of point": 0};
+  calculationsChecked: {[key: string]: boolean}={"mean": false, "median": true, "mode": true,"standard deviation": true, "variance": true,"p-value": true,"correlation between parameters": true, "percentile of point": true};
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  checkChange(s: string){
+    if(this.calculationsChecked[s]){
+      this.calculationsChecked[s]= false;
+    }else{
+      this.calculationsChecked[s]= true;
+    }
+ }
 }
